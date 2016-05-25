3eb725410d4d0bb1073e4bf7a61f7f0b4ec9ca40
doniyusdinar <doniyusdinar@gmail.com>
2016-05-25 14:13:22 +0700

    develop pertama

---
+++ AvionicsInstrumentsControls/MenuView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework.Controls;
+using MetroFramework;
+
+namespace MissionPlanner.AvionicsInstrumentsControls
+{
+    public partial class MenuView : MetroForm
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
